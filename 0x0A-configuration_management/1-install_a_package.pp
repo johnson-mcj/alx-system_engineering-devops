42c2a10a00cf90e99fac2ef1933d3cde6253fb61
johnson-mcj <musukwajohnson5@gmail.com>
2023-08-25 13:49:39 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install puppet-lint
-package { 'puppet-lint':
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Notify the user about the installed version
+notify { 'Installed Flask version':
+  message => "Flask 2.1.0 installed",
 }
